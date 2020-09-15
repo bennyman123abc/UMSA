9ca90063b840eb90d445809bacdffa3309939ef4
bennyman123abc <bennyman123abc@hotmail.com>
2020-09-15 16:40:20 -0500

    Initial Commit

---
+++ src/main/resources/archetype-resources/src/main/java/__mainClass__.java
@@ -0,0 +1,18 @@
+#set( $symbol_dollar = '$' )
+package ${package};
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ${mainClass} extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
